58b310c94e104ecad04d655cec87e83f65828e30
Sergey Dunets <dunets.devel@gmail.com>
2012-08-12 06:59:46 +0300

    add IAPosition class

---
+++ Imagine Alice/IAPosition.h
@@ -0,0 +1,18 @@
+//
+//  IAPosition.h
+//  Imagine Alice
+//
+//  Created by Sergey Dunets on 8/12/12.
+//  Copyright (c) 2012 imagine-alice.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Left hand bottom corner is x=0, y=0
+
+@interface IAPosition : NSObject
+
+@property (assign, nonatomic) NSInteger x;
+@property (assign, nonatomic) NSInteger y;
+
+@end
---
+++ Imagine Alice/IAPosition.m
@@ -0,0 +1,16 @@
+//
+//  IAPosition.m
+//  Imagine Alice
+//
+//  Created by Sergey Dunets on 8/12/12.
+//  Copyright (c) 2012 imagine-alice.org. All rights reserved.
+//
+
+#import "IAPosition.h"
+
+@implementation IAPosition
+
+@synthesize x = _x;
+@synthesize y = _y;
+
+@end
