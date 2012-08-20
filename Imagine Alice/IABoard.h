e1963fb62a53ac433a6713e52ee7a7ac5fb0712f
Sergey Dunets <dunets.devel@gmail.com>
2012-08-21 01:18:04 +0300

    add IABoard

---
+++ Imagine Alice/IABoard.h
@@ -0,0 +1,19 @@
+//
+//  IABoard.h
+//  ImagineAlice-iPhone
+//
+//  Created by Sergey Dunets on 7/29/12.
+//  Copyright (c) 2012 imagine-alice.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IABoard : NSObject
+
+@property (assign, nonatomic) NSUInteger numCols;
+@property (assign, nonatomic) NSUInteger numRows;
+
+- (id)initWithNumCols:(NSUInteger)numCols numRows:(NSUInteger)numRows;
+- (id)init;
+
+@end
---
+++ Imagine Alice/IABoard.m
@@ -0,0 +1,44 @@
+//
+//  IABoard.m
+//  ImagineAlice-iPhone
+//
+//  Created by Sergey Dunets on 7/29/12.
+//  Copyright (c) 2012 imagine-alice.org. All rights reserved.
+//
+
+#import "IABoard.h"
+
+const NSUInteger kMaxBoardSize = 16;
+const NSUInteger kMinBoardSize = 3;
+const NSUInteger kDefaultBoardSize = 3;
+
+@implementation IABoard
+
+@synthesize numCols = _numCols;
+@synthesize numRows = _numRows;
+
+// Designated initializer.
+// It checks if values are valid and if they are not, it uses defaults.
+- (id)initWithNumCols:(NSUInteger)numCols numRows:(NSUInteger)numRows
+{
+    if (self = [super init]) 
+    {
+        if (_numCols >= kMinBoardSize && _numCols <= kMaxBoardSize)
+            _numCols = numCols;
+        else
+            _numCols = kDefaultBoardSize;
+        
+        if (_numRows >= kMinBoardSize && _numRows <= kMaxBoardSize)
+            _numRows = numRows;
+        else
+            _numRows = kDefaultBoardSize;
+    }
+    return self;
+}
+
+- (id)init
+{
+    return [self initWithNumCols:kDefaultBoardSize numRows:kDefaultBoardSize];
+}
+
+@end
