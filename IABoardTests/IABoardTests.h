e17e8868e05af6a4555e5f2132e99e2c6e88100f
Sergey Dunets <dunets.devel@gmail.com>
2012-08-22 02:24:00 +0300

    add IABoardTests

---
+++ IABoardTests/IABoardTests.h
@@ -0,0 +1,13 @@
+//
+//  IABoardTests.h
+//  IABoardTests
+//
+//  Created by Sergey Dunets on 8/22/12.
+//  Copyright (c) 2012 imagine-alice.org. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IABoardTests : SenTestCase
+
+@end
---
+++ IABoardTests/IABoardTests.m
@@ -0,0 +1,97 @@
+//
+//  IABoardTests.m
+//  IABoardTests
+//
+//  Created by Sergey Dunets on 8/22/12.
+//  Copyright (c) 2012 imagine-alice.org. All rights reserved.
+//
+
+#import "IABoardTests.h"
+#import "IABoard.h"
+
+@implementation IABoardTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+/*
+- (id)initWithNumCols:(NSUInteger)numCols numRows:(NSUInteger)numRows;
+- (id)init;
+- (BOOL)isPositionOnBoard:(IAPosition *)position;*/
+
+- (void)testInit
+{
+    IABoard *testBoard = [[IABoard alloc] init];
+    STAssertNotNil(testBoard, @"testBoard is nil");
+    STAssertEquals(testBoard.numCols, 3u, @"Default numCols is wrong");
+    STAssertEquals(testBoard.numRows, 3u, @"Default numRows is wrong");
+    [testBoard release];
+}
+
+- (void)testInitWithNumColsNumRows
+{
+    IABoard *testBoard = [[IABoard alloc] initWithNumCols:5 numRows:5];
+    STAssertNotNil(testBoard, @"testBoard is nil");
+    STAssertEquals(testBoard.numCols, 5u, @"Default numCols is wrong");
+    STAssertEquals(testBoard.numRows, 5u, @"Default numRows is wrong");
+    [testBoard release];
+}
+
+- (void)testInitWithMinNumColsNumRows
+{
+    IABoard *testBoard = [[IABoard alloc] initWithNumCols:3 numRows:3];
+    STAssertNotNil(testBoard, @"testBoard is nil");
+    STAssertEquals(testBoard.numCols, 3u, @"Default numCols is wrong");
+    STAssertEquals(testBoard.numRows, 3u, @"Default numRows is wrong");
+    [testBoard release];
+}
+
+- (void)testInitWithMaxNumColsNumRows
+{
+    IABoard *testBoard = [[IABoard alloc] initWithNumCols:10 numRows:10];
+    STAssertNotNil(testBoard, @"testBoard is nil");
+    STAssertEquals(testBoard.numCols, 10u, @"Default numCols is wrong");
+    STAssertEquals(testBoard.numRows, 10u, @"Default numRows is wrong");
+    [testBoard release];
+}
+
+- (void)testInitWithLargeNumColsNumRows
+{
+    IABoard *testBoard = [[IABoard alloc] initWithNumCols:11 numRows:11];
+    STAssertNotNil(testBoard, @"testBoard is nil");
+    STAssertEquals(testBoard.numCols, 3u, @"Default numCols is wrong");
+    STAssertEquals(testBoard.numRows, 3u, @"Default numRows is wrong");
+    [testBoard release];
+}
+
+- (void)testInitWithSmallNumColsNumRows
+{
+    IABoard *testBoard = [[IABoard alloc] initWithNumCols:2 numRows:2];
+    STAssertNotNil(testBoard, @"testBoard is nil");
+    STAssertEquals(testBoard.numCols, 3u, @"Default numCols is wrong");
+    STAssertEquals(testBoard.numRows, 3u, @"Default numRows is wrong");
+    [testBoard release];
+}
+
+- (void)testInitWithNegativeNumColsNumRows
+{
+    IABoard *testBoard = [[IABoard alloc] initWithNumCols:-1 numRows:-1];
+    STAssertNotNil(testBoard, @"testBoard is nil");
+    STAssertEquals(testBoard.numCols, 3u, @"Default numCols is wrong");
+    STAssertEquals(testBoard.numRows, 3u, @"Default numRows is wrong");
+    [testBoard release];
+}
+
+
+
+@end
