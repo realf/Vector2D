d2a1f1595c3b65be097a40942788bd18455d390a
Sergey Dunets <dunets.devel@gmail.com>
2012-08-22 01:32:53 +0300

    add IACharacterTests

---
+++ IACharacterTests/IACharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  IACharacterTests.h
+//  IACharacterTests
+//
+//  Created by Sergey Dunets on 8/22/12.
+//  Copyright (c) 2012 imagine-alice.org. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IACharacterTests : SenTestCase
+
+@end
---
+++ IACharacterTests/IACharacterTests.m
@@ -0,0 +1,50 @@
+//
+//  IACharacterTests.m
+//  IACharacterTests
+//
+//  Created by Sergey Dunets on 8/22/12.
+//  Copyright (c) 2012 imagine-alice.org. All rights reserved.
+//
+
+#import "IACharacterTests.h"
+#import "IACharacter.h"
+
+@implementation IACharacterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testInit
+{
+    IACharacter *testCharacter = [[IACharacter alloc] init];
+    STAssertNotNil(testCharacter, @"testCharacter is nil");
+    STAssertEqualObjects(testCharacter.name, @"Default Name", @"Wrong default name");
+    STAssertTrue(testCharacter.position.x == 0 && testCharacter.position.y == 0, @"Wrong default position");
+    STAssertTrue(testCharacter.isEnabled, @"Wrong default isEnabled state");
+    [testCharacter release];
+}
+
+- (void)testInitWithNamePosition
+{
+    IACharacter *testCharacter = [[IACharacter alloc] initWithName:@"Alice" position:[IAPosition newPositionWithX:1 y:1]];
+    STAssertNotNil(testCharacter, @"testCharacter is nil");
+    STAssertEqualObjects(testCharacter.name, @"Alice", @"Wrong default name");
+    STAssertTrue(testCharacter.position.x == 1 && testCharacter.position.y == 1, @"Wrong default position");
+    STAssertTrue(testCharacter.isEnabled, @"Wrong default isEnabled state");
+    [testCharacter release];
+}
+
+
+
+@end
