9afc240710f997f9bb9819e3c56ef55c5d8e3666
Sergey Dunets <dunets.devel@gmail.com>
2012-09-16 15:17:04 +0300

    add IAHistoryPoint

---
+++ Imagine Alice/IAHistoryPoint.h
@@ -0,0 +1,15 @@
+//
+//  IAHistoryPoint.h
+//  Imagine Alice
+//
+//  Created by Sergey Dunets on 9/16/12.
+//  Copyright (c) 2012 imagine-alice.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IAHistoryPoint : NSObject
+
+- (void)addObjectToHistoryPoint:(id)object;
+
+@end
---
+++ Imagine Alice/IAHistoryPoint.m
@@ -0,0 +1,38 @@
+//
+//  IAHistoryPoint.m
+//  Imagine Alice
+//
+//  Created by Sergey Dunets on 9/16/12.
+//  Copyright (c) 2012 imagine-alice.org. All rights reserved.
+//
+
+#import "IAHistoryPoint.h"
+
+@interface IAHistoryPoint()
+
+@property (retain, nonatomic) NSMutableArray *historyPoint;
+
+@end
+
+@implementation IAHistoryPoint
+
+@synthesize historyPoint = _historyPoint;
+
+- (NSMutableArray *)historyPoint
+{
+    if (!_historyPoint)
+    {
+        _historyPoint = [[NSMutableArray alloc] init];
+    }
+    return _historyPoint;
+}
+
+- (void)addObjectToHistoryPoint:(id)object
+{
+    if (object != nil)
+    {
+        [self.historyPoint addObject:object];
+    }
+}
+
+@end
