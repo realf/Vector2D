b0f39b184e321e73497ac278295bf66ae652208a
Sergey Dunets <dunets.devel@gmail.com>
2012-09-16 19:03:14 +0300

    add IAGlobalConstants.h

---
+++ Imagine Alice/IAGlobalConstants.h
@@ -0,0 +1,14 @@
+//
+//  IAGlobalConstants.h
+//  Imagine Alice
+//
+//  Created by Sergey Dunets on 9/16/12.
+//  Copyright (c) 2012 imagine-alice.org. All rights reserved.
+//
+
+#ifndef Imagine_Alice_IAGlobalConstants_h
+#define Imagine_Alice_IAGlobalConstants_h
+
+#define EPSILON 1.0e-6
+
+#endif
