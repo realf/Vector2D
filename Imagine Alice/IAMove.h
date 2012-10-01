0919988d38d6c35e7382a5623916583c22302b92
Sergey Dunets <dunets.devel@gmail.com>
2012-10-02 00:13:04 +0300

    add comments to IAMove

--- Imagine Alice/IAMove.h
+++ Imagine Alice/IAMove.h
@@ -8,15 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
-/// Representation of any character's move. 
+/// Representation of any character's move in absolute coordinates.
 #warning "Make the logic clear and delete this ugly class, if possible!"
 
 @interface IAMove : NSObject
 
-// {deltaX=0, deltaY=1} is up; {0, -1} is down; {1, 0} is right; {-1, 0} is left.
+// {deltaX = 0.0, deltaY = 1.0} is up; {0.0, -1.0} is down; {1.0, 0.0} is right; {-1.0, 0.0} is left.
 @property (assign, nonatomic) double deltaX;
 @property (assign, nonatomic) double deltaY;
 @property (retain, nonatomic) NSString *moveDescription;
+
 + (IAMove *)moveByAbsoluteDeltaX:(double)deltaX deltaY:(double)deltaY moveDescription:(NSString *)moveDescription;
 
 @end
